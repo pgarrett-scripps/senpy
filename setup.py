9948233b17cf2d205ea3cb1e27b69ad10d8753c7
pgarrett <pgarrett@scripps.edu>
2022-04-19 12:02:26 -0700

    changed I Lines

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="senpy",
-    version="0.0.1",
+    version="0.0.2",
     author="Patrick Garrett",
     author_email="pgarrett@scripps.edu",
     description="A small package for handling ip2 related files",
